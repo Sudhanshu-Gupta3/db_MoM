b7124e16387021c7ffde2c77a675bd37b83a237b
Sudhanshu <sudhanshug7903@gmail.com>
2024-08-16 22:57:15 +0530

    Change Table Config.

--- Tables/dbo.Registration.sql
+++ Tables/dbo.Registration.sql
@@ -1,25 +1,29 @@
 USE [MoM]
 GO
 
-/****** Object:  Table [dbo].[Registration]    Script Date: 20-07-2024 17:25:40 ******/
+/****** Object:  Table [dbo].[Tblusermaster]    Script Date: 16-08-2024 22:55:49 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Registration](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[Tblusermaster](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
 	[UserName] [varchar](100) NULL,
 	[Password] [varchar](100) NULL,
 	[Email] [varchar](100) NULL,
 	[Phone-Number] [bigint] NULL,
 	[IsActive] [int] NULL,
+	[GlobalUserId] [uniqueidentifier] NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID] ASC
+	[UserID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tblusermaster] ADD  DEFAULT (newid()) FOR [GlobalUserId]
 GO
 
 
